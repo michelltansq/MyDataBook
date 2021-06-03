b2d0fd313de71383cf8d62e93c9078fcfd020be9
Michell <19030685@myrp.edu.sg>
2021-06-03 22:10:20 +0800

    Edited activity_main.xml

--- app/src/main/java/sg/edu/rp/webservices/mydatabook/vaccinationfragment.java
+++ app/src/main/java/sg/edu/rp/webservices/mydatabook/vaccinationfragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link VaccinationFragment#newInstance} factory method to
+ * Use the {@link vaccinationfragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class vaccinationfragment extends Fragment {
