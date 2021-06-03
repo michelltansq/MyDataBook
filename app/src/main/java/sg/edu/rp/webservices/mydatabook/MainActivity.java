f9cda6aa1b34429a3ac6c52ca1a4f2068b8dda4c
Michell <19030685@myrp.edu.sg>
2021-06-03 20:38:07 +0800

    Updated files

--- app/src/main/java/sg/edu/rp/webservices/mydatabook/MainActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/mydatabook/MainActivity.java
@@ -1,14 +1,120 @@
 package sg.edu.rp.webservices.mydatabook;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBarDrawerToggle drawerToggle;
+    private String[] drawerItems;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    ArrayAdapter<String> aa;
+    String currentTitle;
+    ActionBar ab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerList = findViewById(R.id.left_drawer);
+        drawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout,      /* DrawerLayout object */
+                R.string.drawer_open, /* "open drawer" description */
+                R.string.drawer_close /* "close drawer" description */
+        ) {
+
+            /** Would be called when a drawer has completely closed */
+            @Override
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                ab.setTitle(currentTitle);
+            }
+
+            /** Would be called when a drawer has completely open */
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                ab.setTitle("My Data Book");
+            }
+        };
+
+        ab = getSupportActionBar();
+        currentTitle = this.getTitle().toString();
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        ab.setDisplayHomeAsUpEnabled(true);
+        drawerItems = new String[]{"Bio", "Vaccination", "Anniversary","About Us"};
+        ab = getSupportActionBar();
+
+        aa = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_activated_1, drawerItems);
+        drawerList.setAdapter(aa);
+
+        // Set the list's click listener
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+
+                Fragment fragment = null;
+                if (position == 0)
+                    fragment = new biofragment();
+                else if (position == 1)
+                    fragment = new vaccinationfragment();
+                else if (position == 2)
+                    fragment = new anniversaryfragment();
+                else if (position == 3)
+                    fragment = new aboutusfragment();
+
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction trans = fm.beginTransaction();
+                trans.replace(R.id.content_frame, fragment);
+                trans.commit();
+
+                // Highlight the selected item,
+                //  update the title, and close the drawer
+                drawerList.setItemChecked(position,true);
+                currentTitle = drawerItems[position];
+                ab.setTitle(currentTitle);
+                drawerLayout.closeDrawer(drawerList);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 }
