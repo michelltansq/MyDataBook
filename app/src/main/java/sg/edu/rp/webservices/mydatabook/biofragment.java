899f7752ee8da0676399902c7a82f6fc95be30b2
Shawn <19040010@rp.edu.sg>
2021-06-03 15:09:46 +0800

    Updated biofragment.xml

--- app/src/main/java/sg/edu/rp/webservices/mydatabook/biofragment.java
+++ app/src/main/java/sg/edu/rp/webservices/mydatabook/biofragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link biofragment#newInstance} factory method to
+ * Use the {@link BioFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class biofragment extends Fragment {
+public class BioFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class biofragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public biofragment() {
+    public BioFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class biofragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment biofragment.
+     * @return A new instance of fragment BioFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static biofragment newInstance(String param1, String param2) {
-        biofragment fragment = new biofragment();
+    public static BioFragment newInstance(String param1, String param2) {
+        BioFragment fragment = new BioFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
--- app/src/main/java/sg/edu/rp/webservices/mydatabook/vaccinationfragment.java
+++ app/src/main/java/sg/edu/rp/webservices/mydatabook/vaccinationfragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link vaccinationfragment#newInstance} factory method to
+ * Use the {@link VaccinationFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class vaccinationfragment extends Fragment {
+public class VaccinationFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class vaccinationfragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public vaccinationfragment() {
+    public VaccinationFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class vaccinationfragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment vaccinationfragment.
+     * @return A new instance of fragment VaccinationFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static vaccinationfragment newInstance(String param1, String param2) {
-        vaccinationfragment fragment = new vaccinationfragment();
+    public static VaccinationFragment newInstance(String param1, String param2) {
+        VaccinationFragment fragment = new VaccinationFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
